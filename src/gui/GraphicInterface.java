0060dea471b60432461d471118d3630326cba028
jacoboqc <jacoboqc@gmail.com>
2016-09-30 21:10:41 +0200

    First try at 'about' popup.

--- src/gui/GraphicInterface.java
+++ src/gui/GraphicInterface.java
@@ -1,24 +1,37 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 public class GraphicInterface extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start (Stage stage) {
         stage.setTitle("PSI Game 2016 0.1");
         Scene scene = new Scene(new VBox(), 1000, 850);
+        MenuBar menuBar = drawMenuBar(stage);
+        ((VBox) scene.getRoot()).getChildren().addAll(menuBar);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private MenuBar drawMenuBar (Stage stage) {
         MenuBar menuBar = new MenuBar();
 
         Menu menuFile = new Menu("File");
         MenuItem exit = new MenuItem("Exit");
-        exit.setOnAction(t -> System.exit(0));
+        exit.setOnAction(event -> System.exit(0));
         menuFile.getItems().add(exit);
 
         Menu menuEdit = new Menu("Edit");
@@ -37,17 +50,22 @@ public class GraphicInterface extends Application {
         menuWindow.getItems().add(verbose);
 
         Menu menuHelp = new Menu("Help");
-        MenuItem help = new MenuItem("Help");
-        menuHelp.getItems().add(help);
+        MenuItem about = new MenuItem("About");
+        about.setOnAction(event -> {
+            Popup popup = new Popup();
+            popup.setX(300);
+            popup.setY(200);
+            popup.setAutoHide(true);
+            popup.getContent().addAll(new Circle(25, 25, 50, Color.AQUAMARINE), new Label("About screen."));
+            popup.show(stage);
+        });
+        menuHelp.getItems().add(about);
 
         menuBar.getMenus().addAll(menuFile, menuEdit, menuRun, menuWindow, menuHelp);
-
-        ((VBox) scene.getRoot()).getChildren().addAll(menuBar);
-        stage.setScene(scene);
-        stage.show();
+        return menuBar;
     }
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         launch(args);
     }
 }
