f91fad4299b3124bf45a1eca29423d24a1d4c27d
jacoboqc <jacoboqc@gmail.com>
2016-10-26 19:43:43 +0200

    Fixed agent registers.

---
+++ src/agent/Fixed.java
@@ -0,0 +1,34 @@
+package agent;
+
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+
+public class Fixed extends Agent {
+    @Override
+    protected void setup() {
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("Player");
+        sd.setName("Fixed player");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+        System.out.println("Hello! " + getAID().getName() + "is ready.");
+    }
+
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+        System.out.println("Agent " + getAID().getName() + " terminating.");
+    }
+}
