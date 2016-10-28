c59716b93cdf619b48b32bcde034527d40f80ab1
jacoboqc <jacoboqc@gmail.com>
2016-10-28 19:06:55 +0200

    Send INFORM with player id and game parameters.

--- src/agent/Fixed.java
+++ src/agent/Fixed.java
@@ -1,14 +1,65 @@
 package agent;
 
 import jade.core.Agent;
+import jade.core.behaviours.WakerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
 
 public class Fixed extends Agent {
+    private int id; //Id
+    private int numberPlayers; //N
+    private int matrixSize; //S
+    private int numberRounds; //R
+    private int numberIterations; //I
+    private int percentage; //P
+
     @Override
     protected void setup() {
+        System.out.print("> Registering in the DF... ");
+        register();
+        System.out.println("Done!");
+
+        addBehaviour(new WakerBehaviour(this, 0) {
+            protected void handleElapsedTimeout() {
+                System.out.print("> Receiving message... ");
+                ACLMessage msg = myAgent.blockingReceive();
+                System.out.print("Done! ");
+                if (msg != null) {
+                    System.out.print("Processing message... ");
+                    processInform(msg);
+                    System.out.println("Done!");
+                    System.out.println("This is PLAYER #" + id);
+                    System.out.println("The game is " + numberPlayers + " players, " + matrixSize + " matrix size, " + numberRounds + " number of rounds, " + numberIterations + " number of iterations and " + percentage + " percentage.");
+                }
+            }
+        });
+        System.out.println("Hello! " + getAID().getName() + "is ready.");
+    }
+
+    private void processInform(ACLMessage msg) {
+        String content = msg.getContent();
+        id = Integer.parseInt(content.split("#")[1]);
+        String[] parameters = content.split("#")[2].split(",");
+        numberPlayers = Integer.parseInt(parameters[0]);
+        matrixSize = Integer.parseInt(parameters[1]);
+        numberRounds = Integer.parseInt(parameters[2]);
+        numberIterations = Integer.parseInt(parameters[3]);
+        percentage = Integer.parseInt(parameters[4]);
+    }
+
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+        System.out.println("Agent " + getAID().getName() + " terminating.");
+    }
+
+    private void register () {
         DFAgentDescription dfd = new DFAgentDescription();
         dfd.setName(getAID());
         ServiceDescription sd = new ServiceDescription();
@@ -20,15 +71,5 @@ public class Fixed extends Agent {
         } catch (FIPAException fe) {
             fe.printStackTrace();
         }
-        System.out.println("Hello! " + getAID().getName() + "is ready.");
-    }
-
-    protected void takeDown() {
-        try {
-            DFService.deregister(this);
-        } catch (FIPAException fe) {
-            fe.printStackTrace();
-        }
-        System.out.println("Agent " + getAID().getName() + " terminating.");
     }
 }
--- src/agent/Main.java
+++ src/agent/Main.java
@@ -7,32 +7,29 @@ import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main extends Agent{
-    private int numberPlayers = 6;
-    private int[][][] matrix = new int[numberPlayers][numberPlayers][2];
+    private int numberPlayers = 6; //N
+    private int matrixSize = 3; //S
+    private int numberRounds = 2; //R
+    private int numberIterations = 5; //I
+    private int percentage = 25; //P
+    private int[][][] matrix = new int[matrixSize][matrixSize][2];
+
     @Override
     protected void setup() {
-        int payoff1, payoff2;
-        Random r = new Random();
-        for (int i = 0; i < numberPlayers; i++) {
-            for (int j = i; j < numberPlayers; j++) {
-                payoff1 = r.nextInt(10);
-                matrix[i][j][0] = payoff1;
-                matrix[j][i][1] = payoff1;
-                payoff2 = r.nextInt(10);
-                matrix[i][j][1] = payoff2;
-                matrix[j][i][0] = payoff2;
-            }
-        }
+        createMatrix();
         printMatrix();
-
-        addBehaviour(new WakerBehaviour(this, 15000) {
+        System.out.println("> Press ENTER to begin.");
+        new Scanner(System.in).nextLine();
+        addBehaviour(new WakerBehaviour(this, 0) {
             private AID[] players;
             protected void handleElapsedTimeout() {
-                System.out.println("> Searching...");
+                System.out.print("> Searching for agents... ");
                 DFAgentDescription template = new DFAgentDescription();
                 ServiceDescription sd = new ServiceDescription();
                 sd.setType("Player");
@@ -47,6 +44,16 @@ public class Main extends Agent{
                 } catch (FIPAException e) {
                     e.printStackTrace();
                 }
+                System.out.println("Done!");
+
+                System.out.print("> Informing agents... ");
+                for (int i = 0; i < players.length; i++) {
+                    ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+                    msg.addReceiver(players[i]);
+                    msg.setContent("Id#" + i + "#" + numberPlayers + "," + matrixSize + "," + numberRounds + "," + numberIterations + "," + percentage);
+                    send(msg);
+                }
+                System.out.println("Done!");
             }
         });
         System.out.println("Hello! " + getAID().getName() + "is ready.");
@@ -56,9 +63,24 @@ public class Main extends Agent{
         System.out.println("Agent " + getAID().getName() + " terminating.");
     }
 
+    private void createMatrix () {
+        int payoff1, payoff2;
+        Random r = new Random();
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = i; j < matrixSize; j++) {
+                payoff1 = r.nextInt(10);
+                matrix[i][j][0] = payoff1;
+                matrix[j][i][1] = payoff1;
+                payoff2 = r.nextInt(10);
+                matrix[i][j][1] = payoff2;
+                matrix[j][i][0] = payoff2;
+            }
+        }
+    }
+
     private void printMatrix () {
-        for (int i = 0; i < numberPlayers; i++) {
-            for (int j = 0; j < numberPlayers; j++) {
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
                 System.out.print(matrix[i][j][0] + "/" + matrix[i][j][1] + " ");
             }
             System.out.println();
